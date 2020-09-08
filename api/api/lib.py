7d4e814b2ed9a1e7ce8c17f1f0b078b6cd3163f2
Dominic Henry <dominichenrywork@hotmail.com>
2020-09-08 08:09:33 -0500

    Update test_lib.py

--- api/api/lib.py
+++ api/api/lib.py
@@ -16,19 +16,50 @@ def get_ip(request):
     ip_address = request.remote_addr
     return ip_address
 
-def ip_exists(ip_address):
+def ip_exists(ip_address, client):
     """Determines whether an IP address already exists in database
 
     Args:
         ip_address
+        client: pymongo client used to connect to mongodb database
 
     Return:
         exists: boolean indicating whether an IP address exists in database
 
     """
+    user_collection = client.db.user
+    exists = bool(user_collection.find_one({'ipAddress': ip_address}))
+    return exists
+
+def __generate_report(user):
+    """Generates required report for a given user
+
+    Note:
+        expenses < (income + savings) - You have a good handle of your budget
+        expenses > (income + savings) - You need to cut back on your expenses
+        expenses = (income + savings) - You budget is very tight
+
+    Args:
+        user: dictionary containing user data
+
+    Return:
+        report: dictionary containing additional reported information
+
+        report = {
+            'firstName': 'Dominic',
+            'lastName': 'Henry',
+            ...
+            totalExpenses: 50000
+            totalSavings: 170000
+            totalIncome: 300000
+            leftover: 80000,
+            statement: 'You have a good handle of your budget'
+        }
+
+    """
     pass
 
-def insert_user(user_data, ip_address):
+def insert_user(user_data, ip_address, client):
     """Inserts new user data into database
 
     Required Submission data from user_data:
@@ -42,6 +73,7 @@ def insert_user(user_data, ip_address):
     Args:
         user_data: json object containing all user data
         ip_address: ip address of the user
+        client: pymongo client used to connect to mongodb database
 
     Return:
         user_id: valid user_id or None
@@ -51,32 +83,15 @@ def insert_user(user_data, ip_address):
     """
     pass
 
-def all_users():
+def all_users(client):
     """Retrieves all user data
 
     Args:
-        None
+        client: pymongo client used to connect to mongodb database
 
     Return:
         users: json object containing a all user data denoted by their
         associated id
-
-    """
-    pass
-
-def generate_report(user_id):
-    """Generates required report for a given user
-
-    Note:
-        expenses < (income + savings) - You have a good handle of your budget
-        expenses > (income + savings) - You need to cut back on your expenses
-        expenses < (income + savings) - You budget is very tight
-
-    Args:
-        user_id
-
-    Return:
-        all_users: json object containing summary of user information
 
     """
     pass
--- api/unittests/test_lib.py
+++ api/unittests/test_lib.py
@@ -2,9 +2,12 @@
 import unittest
 import os
 import sys
+import json
+import random
 
 # Third-party modules
 from mongomock import MongoClient
+from bson.objectid import ObjectId
 
 # Flask Imports
 from flask import Flask
@@ -16,7 +19,39 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 # Application imports
 from api.lib import (get_ip, ip_exists, insert_user, all_users,
-                       generate_report)
+                       __generate_report)
+
+def create_user_dict(firstName, lastName):
+    """Creates a new user dictionary for testing
+
+    Args:
+        firstName
+        lastName
+
+    Return:
+        user: dictionary containing random user data
+
+    """
+    user_one = {
+        'firstName': firstName,
+        'lastName': lastName,
+        'expense': {
+            'grocery': random.randrange(20000, 25000),
+            'utilities': random.randrange(5000, 10000),
+            'transportation': random.randrange(5000, 10000)
+        },
+        'income': {
+            'salary': random.randrange(200000, 6000000),
+        },
+        'savings': {
+            'equities': random.randrange(10000, 30000),
+            'pension': random.randrange(10000, 20000),
+            'emergency': random.randrange(10000, 30000)
+        }
+    }
+
+    return user_one
+
 
 class TestLibraryModules(unittest.TestCase):
     """Tests Each method stored in the lib.py module"""
@@ -24,13 +59,27 @@ class TestLibraryModules(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         """Test class setup"""
+
+        # Need a new app to test various functionality surrounding processing
+        # request objects
         self.app = Flask(__name__)
         self.client = self.app.test_client()
         self.client.testing = True
 
-        user_test_collection = MongoClient.db.collection()
+    def setUp(self):
+        """Test setup upon each run"""
 
+        # Creating user instance
+        self.user = create_user_dict('Dominic', 'Henry')
+        self.user['ipAddress'] = '192.0.1.20'
 
+        # Instantiate MongoClient
+        # Inserting new user instance into mock mongodb database
+        self.mongo_client = MongoClient()
+        self.user_collection = self.mongo_client.db.user
+
+        # Insert new user
+        self.user['_id'] = self.user_collection.insert_one(self.user).inserted_id
 
     def test_get_ip(self):
         """Tests get_ip"""
@@ -39,8 +88,59 @@ class TestLibraryModules(unittest.TestCase):
         request = type('Request', (object,), {'remote_addr': '192.0.1.20'})()
         self.assertEqual(get_ip(request), request.remote_addr)
 
-    def ip_exists(self):
+    def test_ip_exists(self):
         """Test ip_exists"""
+
+        # Testing when ip address exists within mongodb database
+        result = ip_exists(self.user['ipAddress'], self.mongo_client)
+        self.assertTrue(result)
+
+        # Testing when ip address does not exist within mongodb database
+        result = ip_exists('127.0.10.10', self.mongo_client)
+        self.assertFalse(result)
+
+    def test___generate_report(self):
+        "Test generate_report"
+
+        # Case One: expenses < (income + savings)
+
+        # Case Two: expenses > (income + savings)
+        # Case Three: expenses = (income + savings)
+
+
+    def test_insert_user(self):
+        """Test insert_user"""
+
+        # Tests that a valid user is entered correctly
+        user_one = create_user_dict('Gabrielle', 'Clarke')
+        user_id, message, status_code = insert_user(json.dumps(user_one),
+                                                    '191.0.10.10',
+                                                    self.mongo_client)
+
+        self.assertEqual(ObjectId, type(user_id))
+        self.assertEqual('Success', message)
+        self.assertEqual(200, status_code)
+
+        # Test that a invalid user is not entered
+        user_two = {}
+        user_id, message, status_code = insert_user(json.dumps(user_two),'193.0.10.10', self.mongo_client)
+
+        self.assertIsNone(user_id)
+        self.assertEqual('Failed', message)
+        self.assertEqual('400', status_code)
+
+    def test_all_users(self):
+        """Test all_users"""
+
+        # Inserting additional users for testing
+        users = [create_user_dict('Alex', 'Lawson'), create_user_dict('Dineah',
+                                                                      'Cohen')]
+        user_ids = self.user_collection.insert_many(users).inserted_ids
+        expected = [{**users[i], **dict(zip(['_id'], [user_ids[i]]))} for i in
+                 range(len(users))]
+
+        result = all_users(self.mongo_client)
+        self.assertEqual(expected, result)
 
 
 if __name__ == "__main__":
