855d7aec12fd4e5e59a6c79e3281c4216e7464ec
Dominic Henry <dominichenrywork@hotmail.com>
2020-09-11 13:36:46 -0500

    Minor fixes to lib.py imports

--- api/api/lib.py
+++ api/api/lib.py
@@ -8,8 +8,13 @@ import json
 import mongomock
 import pymongo
 
+# PYTHONPATH
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
 # API Imports
-from constants import required, optional
+from api.constants import required, optional
 
 # TODO verify that request passes valid ip address
 def get_ip(request):
