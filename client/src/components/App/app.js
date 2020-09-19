94b8085e9db7244d35c9b3c8d2aa3fff0fa4da97
Dominic Henry <dominichenrywork@hotmail.com>
2020-09-19 00:02:06 -0500

    Updated tests for app.test.js

--- client/src/components/App/app.js
+++ client/src/components/App/app.js
@@ -6,16 +6,26 @@ import { Tabs } from "../Tabs";
 import { EntryList } from "../EntryList/entrylist";
 
 // Utilities Import
-import { allUsers } from "../../utils/request";
+import { create, allUsers } from "../../utils/request";
 import { userCreate, categories } from "../../utils/userCreate";
 
 // Assets Imports
 import "./app.css";
 
+const tabs = [...categories, "all"];
+
 function App() {
+  const [tab, setTab] = useState(0);
+  const [user, setUser] = useState(userCreate());
+  const [dest, setDest] = useState();
+  const [amount, setAmount] = useState();
   return (
     <div className="App">
       <h1>Budget Calculator</h1>
+      <div className="">
+        <input data-testid="desc" placeholder="Description" />
+        <input data-testid="amount" placeholder="Amount" />
+      </div>
     </div>
   );
 }
--- client/src/components/App/app.test.js
+++ client/src/components/App/app.test.js
@@ -1,10 +1,59 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./app";
 
-describe("<App />", () => {
-  test("renders learn react link", () => {
+describe("<App /> Application Title", () => {
+  test("Ensures that App title is rendered", () => {
     render(<App />);
     expect(screen.getByText("Budget Calculator")).toBeInTheDocument();
+  });
+});
+
+describe("<App /> Adding new items to each category", () => {
+  test("Testing that new description and amount fields are updated", () => {
+    // Rendering main app
+    render(<App />);
+
+    // Adding new description and amount
+    const [desc, amount] = [
+      screen.getByTestId("desc"),
+      screen.getByTestId("amount"),
+    ];
+
+    // Adding new values to these input fields
+    userEvent.type(desc, "Salary");
+    userEvent.type(amount, "150000");
+
+    // Assertions
+    expect(desc).toHaveValue("Salary");
+    expect(amount).toHaveValue("150000");
+  });
+
+  test("Testing that when add button is pressed that entrylist is updated", () => {
+    // Rendering main app
+    render(<App />);
+
+    // Adding new description and amount
+    const [desc, amount, add, entrylist] = [
+      screen.getByTestId("desc"),
+      screen.getByTestId("amount"),
+      screen.getByTestId("add"),
+      screen.getByTestId("entrylist"),
+    ];
+
+    // Adding new values to these input fields
+    userEvent.type(desc, "Salary");
+    userEvent.type(amount, "150000");
+
+    // Simulating adding new entry
+    userEvent.click(add);
+
+    // Asserting that input field has been cleared
+    expect(desc).toHaveValue("");
+    expect(amount).toHaveValue("");
+
+    // Asserting that new entry has been added to entrylist
+    expect(entrylist).toHave;
   });
 });
